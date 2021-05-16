2eec0f5d21ab4776b7e158974d50b0a3391104ab
emresandikci <me@emresandikci.com>
2021-05-17 00:25:39 +0300

    chore: illustrations added

--- src/public/images/index.js
+++ src/public/images/index.js
@@ -1,3 +1,5 @@
 export { default as logo } from './Logo.svg';
 export { default as notFound404 } from './404.svg';
 export { default as gift } from './gift.svg';
+export { default as giftCode } from './gift-code.svg';
+export { default as welcome } from './welcome.svg';
