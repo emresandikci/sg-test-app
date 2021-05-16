b6512bae3ee32c105cda7cb7526b6fb4eed98dc7
emresandikci <me@emresandikci.com>
2021-05-16 21:38:41 +0300

    feat: basic styles settings added

--- src/components/globalStyles/index.js
+++ src/components/globalStyles/index.js
@@ -3,18 +3,16 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
   html{
     box-sizing: border-box;
-    line-height: ${({ theme }) => theme.lineHeights.text}px;
-    font-family:${({ theme }) => theme.fonts.default};
-    
+    font-family:${({ theme }) => theme.fonts?.default}; 
+    font-size:${({ theme }) => theme.fontSize}px;
+    background-color:${({ theme }) => theme.background}};
   }
-  
   *, *:before, *:after {
     box-sizing: inherit;
     margin: 0;
     padding:0;
     border:0;
-    font:inherit;
-    font-size:${({ theme }) => theme.fontSizes.normal}px;
+    color:${({ theme }) => theme.fontColor}};
   }
   a{
     text-decoration:none;
